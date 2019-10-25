5f3a3efa03bd910b59a1f30f27d3ceef1027a78f
steinkorsveien <steinkorsveien@localhost>
2019-10-25 11:33:05 +0200

    Added Index Controller

---
+++ src/main/java/com/steinko/Junit5Tutorial/IndexController.java
@@ -0,0 +1,15 @@
+package com.steinko.Junit5Tutorial;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		
+		return "index";
+	}
+
+}
---
+++ src/test/java/com/steinko/Junit5Tutorial/IndexControllerTest.java
@@ -0,0 +1,58 @@
+package com.steinko.Junit5Tutorial;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(IndexController.class)
+
+public class IndexControllerTest {
+	
+	IndexController controller;
+	
+	@BeforeEach
+	void setUp() {
+		controller = new IndexController();
+	}
+	
+	
+		
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void shouldReturnHelloWorld() throws Exception{ 
+		RequestBuilder request = MockMvcRequestBuilders
+				.get("/")
+				.accept(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(request).andReturn();
+		assertEquals("index", result.getResponse().getContentAsString());
+		
+	}
+	
+	@Test 
+	public void shouldReturnHelloWorld2() { 
+		assertEquals("index",controller.index(),()-> "returned index");
+		
+	}
+	
+	
+}
